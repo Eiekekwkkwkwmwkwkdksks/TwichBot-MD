5ae717dbc6739eb3543cf1bc17d1196917e347cd
DiegoBot <151096440+diegojadibot@users.noreply.github.com>
2023-12-09 18:45:03 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
  
-say('Fantasy\nBot\nMD', {
+say('Twich\nBot\nMD', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Project Author:\nWilsmac (@Wilmer)\n\nColaboradores:\nGataNina-Li (@Gata_Dios)`, {
+say(`Project Author:\nDiego (@Diego-Oficial)\n\nColaboradores:\nWilsac (@Wilmer)`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']})
